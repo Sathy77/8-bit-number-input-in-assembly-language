1cf517c3ef3b928e8fec8ceaafd0946df5519b77
Sathy Akter <68398295+Sathy77@users.noreply.github.com>
2020-10-03 12:13:13 +0600

    Update 8 bit number input (user).asm

--- 8 bit number input (user).asm
+++ 8 bit number input (user).asm
@@ -3,44 +3,25 @@
 .DATA
 .CODE
 MAIN PROC  
-    MOV BL, 0     ;BL=0 
-    MOV DL, 0     ;DL=0
-    MOV AL, '0'   ;AL=48
+    MOV BL, 0    
+    MOV DL, 0     
+    MOV AL, '0'   
     
     ABC:  
-    SUB AL, '0'   ;AL=3
-    MOV CL, AL    ;CL=3
-    MOV AL, DL    ;AL=2
+    SUB AL, '0'   
+    MOV CL, AL   
+    MOV AL, DL    
     MOV BL, 10
-    MUL BL        ;AX=20, AL=20
-    ADD AL, CL    ;AL=23
-    MOV DL, AL    ;DL=23
+    MUL BL        
+    ADD AL, CL    
+    MOV DL, AL   
     
     MOV AH, 1     
-    INT 21H       ;AL=51
+    INT 21H    
      
-    CMP AL, ' '
+    CMP AL, 13
     JNE ABC 
-    
-    
-    CMP DL, 255
-    JE OK 
-    
-    
-    MOV AH, 2
-    MOV DL, 'N'
-    INT 21H
-    
-    MOV AH, 4CH
-    INT 21H
-    
-    
-    OK:
-    MOV AH, 2
-    MOV DL, 'Y'
-    INT 21H
-    
-    
+   
     MOV AH, 4CH
     INT 21H
     
@@ -48,4 +29,3 @@ MAIN PROC
     MAIN ENDP
 END MAIN   
 
-;After take input, press spece
